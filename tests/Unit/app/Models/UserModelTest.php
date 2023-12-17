21c86b87d5f03b711a5f7c08a8390fa5f5461458
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 16:28:45 -0300

    test(models): add soft deletes test for user model

--- tests/Unit/app/Models/UserModelTest.php
+++ tests/Unit/app/Models/UserModelTest.php
@@ -9,3 +9,16 @@ it('should make new user model')
 it('should create new user model')
     ->expect(fn () => User::factory()->create())
     ->toBeInstanceOf(User::class);
+
+it('user model should use soft deletes')
+    ->expect(
+        fn () => User::factory()->trashed()->create()
+    )->toBeSoftDeleted();
+
+it('user model should be restored from soft delete')
+    ->expect(
+        fn () => tap(
+            User::factory()->trashed()->create(),
+            fn (User $user) => $user->restore()
+        )
+    )->not()->toBeSoftDeleted();
