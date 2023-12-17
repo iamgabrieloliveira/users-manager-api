149bf53add9b4c2a763bea72efe214560cf7c9e3
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:45:18 -0300

    feat: add "created" helper function in base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function created(int $id): JsonResponse
+    {
+        return response()->json(['id' => $id], Response::HTTP_CREATED);
+    }
 }
