27f84c6cafa04b0ae3e35485b30170d93f210db0
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 16:19:54 -0300

    test(arch): add new rules for architectural tests

--- tests/Feature/ArchitecturalTest.php
+++ tests/Feature/ArchitecturalTest.php
@@ -1,10 +1,37 @@
 <?php
 
+use App\Repositories\Contracts\RepositoryContract;
+
 arch('every file should use strict types enabled')
     ->expect('App')
+    ->classes()
     ->toUseStrictTypes();
 
 arch('avoid use debug statements')
     ->expect(['dd', 'dump', 'ray', 'var_dump'])
     ->not()
     ->toBeUsed();
+
+arch('controllers should have suffix \'Controller\'')
+    ->expect('App\Http\Controllers')
+    ->classes()
+    ->toHaveSuffix('Controller');
+
+arch('avoid use final classes')
+    ->expect('App')
+    ->classes()
+    ->not()
+    ->toBeFinal();
+
+arch('every Data Transfer Object should be readonly class and have suffix DTO and to have constructor')
+    ->expect('App\DataTransferObjects')
+    ->classes()
+    ->toBeReadonly()
+    ->toHaveConstructor()
+    ->toHaveSuffix('DTO');
+
+arch('every repository implementations should have \'Repository\' suffix and implement RespositoryContract')
+    ->expect('App\Repositories\Implementations')
+    ->classes()
+    ->toImplement(RepositoryContract::class)
+    ->toHaveSuffix('Repository');
