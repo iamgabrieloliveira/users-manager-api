1051ad5c5c87ed079d438cd2d18df2d8fabe282d
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:50:50 -0300

    feat(routes): add User management routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('user', UserController::class);
