96498a6166c65e87a8157e774331151d81d86c51
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 16:45:40 -0300

    feat(exceptions): add base application exception

--- tests/Helpers/Http.php
+++ tests/Helpers/Http.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace Tests\Helpers\Http;
 
 use Illuminate\Testing\TestResponse;
+use function Pest\Laravel\delete;
 use function Pest\Laravel\post;
 
 function storeUser(array $payload = []): TestResponse
 {
     return post(route('api.user.store', $payload));
+}
+
+function destroyUser(int $userId): TestResponse
+{
+    return delete(route('api.user.destroy', ['user' => $userId]));
 }
