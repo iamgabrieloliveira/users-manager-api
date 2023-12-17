a3b3f31021b069347ab881c74b30182b05e385bf
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:49:56 -0300

    feat(providers): add repository binds in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\UserRepositoryContract;
+use App\Repositories\Implementations\Eloquent\UserEloquentRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->bindRepositories();
     }
 
     /**
@@ -22,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    private function bindRepositories(): void
+    {
+        $this->app->bind(
+            UserRepositoryContract::class,
+            UserEloquentRepository::class,
+        );
     }
 }
