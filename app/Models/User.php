849235246f2043ed3e562a48ef41842e151a558f
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:20:31 -0300

    feat(models): add soft deletes to User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -40,7 +41,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     protected $fillable = [
         'user_name'.
--- database/migrations/2023_12_17_000000_create_users_table.php
+++ database/migrations/2023_12_17_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('user_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
