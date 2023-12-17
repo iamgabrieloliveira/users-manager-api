321ef16dfd3fd726f1e84ae5fce690f8559dd1b4
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:48:43 -0300

    feat(repositories): create User Eloquent repository

---
+++ app/Repositories/Implementations/Eloquent/UserEloquentRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\Implementations\Eloquent;
+
+use App\DataTransferObjects\User\StoreUserDTO;
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryContract;
+
+class UserEloquentRepository implements UserRepositoryContract
+{
+    public function store(StoreUserDTO $DTO): User
+    {
+        return User::query()->create([
+            'first_name' => $DTO->firstName,
+            'last_name'  => $DTO->lastName,
+            'email'      => $DTO->email,
+            'password'   => $DTO->password,
+            'user_name'  => $DTO->userName,
+        ]);
+    }
+}
