c1394792f27cc60a288f881c6aa4c1af70c27199
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-17 15:50:32 -0300

    test: add create user successfully test

--- tests/Feature/app/Http/Controllers/CreateUserControllerTest.php
+++ tests/Feature/app/Http/Controllers/CreateUserControllerTest.php
@@ -1,5 +1,21 @@
 <?php
 
-it('should create a user successfully')->with('create user valid payloads')->todo();
+declare(strict_types=1);
+
+use function Pest\Laravel\{
+    assertDatabaseHas,
+};
+use function Tests\Helpers\Http\{
+    storeUser,
+};
+
+it('should create a user successfully', function (array $payload) {
+    // Arrange & Act
+    $response = storeUser($payload);
+
+    // Assert
+    $response->assertCreated();
+    assertDatabaseHas('users', ['id' => 1]);
+})->with('create user valid payloads');
 
 it('should not create a user with invalid payload')->with('create user invalid payloads')->todo();
