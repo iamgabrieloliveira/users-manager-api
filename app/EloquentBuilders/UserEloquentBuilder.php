388ca27ffdb18fefa3f8f1ab9afb817405aabf3b
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-18 14:52:36 -0300

    feat(builders): add User specific eloquent builder

---
+++ app/EloquentBuilders/UserEloquentBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EloquentBuilders;
+
+class UserEloquentBuilder extends BaseEloquentBuilder
+{
+    public function filterByName(string $name): BaseEloquentBuilder
+    {
+        $fullName = $this->raw('CONCAT(first_name, \' \', last_name)');
+
+        return $this
+            ->whereLike($fullName, $name)
+            ->orWhereLike('user_name', $name);
+    }
+}
