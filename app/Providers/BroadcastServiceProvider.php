bf98cddf030e3e6bd08e2804489f5ae1e943866a
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-18 20:28:22 -0300

    test(coverage): ignore BroadcastServiceProvider because is not used

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -7,6 +7,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * @codeCoverageIgnore
+ */
 class BroadcastServiceProvider extends ServiceProvider
 {
     /**
