5b37d1a63e6e3a3a772e5a50e738e2e62e2ddb7b
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-18 14:47:56 -0300

    feat(requests): add User index form request

---
+++ app/Http/Requests/IndexUserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexUserRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'q' => ['sometimes'|'string'],
+        ];
+    }
+
+    public function getSearch(): ?string
+    {
+        return $this->validated('q');
+    }
+}
