22705628a4fbc093a2abaf8f44a84a2a6b1f1adf
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-18 20:31:34 -0300

    test: add test for exception handler

---
+++ tests/Unit/app/Exceptions/ExceptionHandlerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Exceptions\UnableToDeleteModelException;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+it('should render UnableToDeleteModelException correctly for models', function (Model $model) {
+    // Arrange
+    $exception = new UnableToDeleteModelException($model);
+
+    // Act & Assert
+    renderException($exception)
+        ->asAssertable()
+        ->assertBadRequest();
+})->with([
+    'User' => fn () => User::factory()->make(),
+])->only();
