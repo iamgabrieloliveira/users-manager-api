bcab3be3cb99bba9238d8386bf3035c380ec8502
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-19 09:30:44 -0300

    refactor: renable UserResource to be more generic

--- app/Resources/UserResource.php
+++ app/Resources/UserResource.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /** @mixin User */
-class UserListResource extends JsonResource
+class UserResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
