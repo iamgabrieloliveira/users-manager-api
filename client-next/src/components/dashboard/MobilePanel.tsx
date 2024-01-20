b3b9da964a20262b36715e6a0e0ef75ef7ad5af1
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-20 17:05:54 -0300

    feat(front/dashboard): add current user mobile panel

---
+++ client-next/src/components/dashboard/MobilePanel.tsx
@@ -0,0 +1,26 @@
+import { Disclosure } from '@headlessui/react';
+import { BellIcon } from '@heroicons/react/24/outline';
+
+type DashboardMobilePanelProps = {
+    user: {
+        username: string,
+        email: string,
+    }
+}
+
+export default function MobilePanel({ user }: DashboardMobilePanelProps) {
+    return (
+        <Disclosure.Panel className="md:hidden">
+            <div className="border-t border-gray-700 pb-3 pt-4">
+                <div className="flex items-center px-5">
+                    <div className="ml-3">
+                        <div
+                            className="text-base font-medium leading-none text-white">{user.username}</div>
+                        <div
+                            className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                    </div>
+                </div>
+            </div>
+        </Disclosure.Panel>
+    );
+}
