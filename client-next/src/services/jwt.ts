86ccb0528b7f1decdff524a1ffb604965f137669
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-20 17:02:07 -0300

    feat(front/auth): create jwt hook to interact with jwt cookie

---
+++ client-next/src/services/jwt.ts
@@ -0,0 +1,30 @@
+import { parseCookies, setCookie, destroyCookie } from 'nookies';
+
+type UseJwt = {
+    setJwt: (value: string) => void;
+    getJwt: () => string|undefined;
+    forgetJwt: () => void;
+}
+
+export default function useJwt(): UseJwt {
+    function setJwt(value: string, maxAge: number) {
+        setCookie(
+            undefined,
+            process.env.JWT_TOKEN_NAME,
+            value,
+            { maxAge }
+        );
+    }
+    
+    function getJwt(): string|undefined {
+        const cookieName = process.env.JWT_TOKEN_NAME;
+        const cookies = parseCookies();
+        return cookies[cookieName];
+    }
+
+    function forgetJwt(): void {
+        destroyCookie(undefined, process.env.JWT_TOKEN_NAME);
+    }
+    
+    return { getJwt, setJwt, forgetJwt };
+}
