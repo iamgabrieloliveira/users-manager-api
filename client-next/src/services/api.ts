419d0d718b27bf96ec49eb20ec7563d8fe396226
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-20 16:57:15 -0300

    feat(front/http): create http client, with interceptor setting token JWT

---
+++ client-next/src/services/api.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+import useJwt from '@/services/jwt';
+
+const api = axios.create({
+    baseURL: process.env.SERVER_URL,
+});
+
+api.interceptors.request.use(function (request) {
+    const { getJwt } = useJwt();
+
+    const token = getJwt();
+
+    if (!token) return request;
+
+    request.headers['Authorization'] = `Bearer ${token}`;
+
+    return request;
+});
+
+const handleErrorWithToast = (error) => {
+    const toastUnknownError = () => toast.error('An error occurred, please refresh your page');
+
+    if (!error instanceof AxiosError) {
+        toastUnknownError();
+        return error;
+    }
+
+    const errors = error.response?.data?.errors;
+
+    if (errors) {
+        errors.forEach(toast.error);
+    } else {
+        toastUnknownError();
+    }
+
+    return error;
+};
+
+export { api, handleErrorWithToast };
