a02d362cd8e016107b5f24f4ed0a54b98cebc932
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-20 17:05:17 -0300

    feat(front/dashboard): add dashboard wrapper

---
+++ client-next/src/components/dashboard/Wrapper.tsx
@@ -0,0 +1,91 @@
+import { useContext } from 'react';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { AuthContext } from '@/contexts/AuthContext';
+import ProfileDropdown from '@/components/ProfileDropdown';
+import MobilePanel from '@/components/dashboard/MobilePanel';
+import MortyIcon from '@/components/icons/MortyIcon';
+
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+];
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
+export default function Wrapper({ children }) {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <>
+            <div className="min-h-full">
+                <Disclosure as="nav" className="bg-gray-800">
+                    {({ open }) => (
+                        <>
+                            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                                <div className="flex h-16 items-center justify-between">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0">
+                                            <MortyIcon size="small"/>
+                                        </div>
+                                        <div className="hidden md:block">
+                                            <div className="ml-10 flex items-baseline space-x-4">
+                                                {navigation.map((item) => (
+                                                    <a
+                                                        key={item.name}
+                                                        href={item.href}
+                                                        className={classNames(
+                                                            item.current
+                                                                ? 'bg-gray-900 text-white'
+                                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                                        )}
+                                                        aria-current={item.current ? 'page' : undefined}
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="hidden md:block">
+                                        <div className="ml-4 flex items-center md:ml-6">
+                                            <ProfileDropdown user={user}/>
+                                        </div>
+                                    </div>
+                                    <div className="-mr-2 flex md:hidden">
+                                        <Disclosure.Button
+                                            className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="absolute -inset-0.5"/>
+                                            <span className="sr-only">Open main menu</span>
+                                            {open ? (
+                                                <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
+                                            ) : (
+                                                <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
+                                            )}
+                                        </Disclosure.Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <MobilePanel user={user}/>
+                        </>
+                    )}
+                </Disclosure>
+
+                <header className="bg-white shadow">
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                    </div>
+                </header>
+                
+                <main>
+                    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+}
