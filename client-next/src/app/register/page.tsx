9e867eab50892fff39d24dd0fdb81b310a47c6da
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-20 16:50:23 -0300

    feat(front/auth): create user registration page

---
+++ client-next/src/app/register/page.tsx
@@ -0,0 +1,153 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { useContext, useState } from 'react';
+import { AuthContext } from '@/contexts/AuthContext';
+import Link from 'next/link';
+import { Button } from '@nextui-org/react';
+import { handleErrorWithToast } from '@/services/api';
+import MortyIcon from '@/components/icons/MortyIcon';
+
+type FormData = {
+    username: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+}
+
+export default function Register() {
+    const { register, handleSubmit } = useForm();
+    const { signUp } = useContext(AuthContext);
+    
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function handleSignUp(data: FormData) {
+        try {
+            setIsLoading(true);
+            await signUp(data);
+        } catch (err) {
+            handleErrorWithToast(err);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <MortyIcon size="medium"/>
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Create your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleSubmit(handleSignUp)}>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                               Username
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('username')}
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    required
+                                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                First name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('firstName')}
+                                    id="firstName"
+                                    name="firstName"
+                                    type="text"
+                                    required
+                                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Last name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('lastName')}
+                                    id="lastName"
+                                    name="lastName"
+                                    type="text"
+                                    required
+                                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('email')}
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    {...register('password')}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <Button
+                                isLoading={isLoading}
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign Up
+                            </Button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Already have account?{' '}
+                        <Link href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Login
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
